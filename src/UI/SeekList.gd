a0acdd7067fc1608985e41fff38ea3a99083d43c
The1Rogue <fabiantschut@gmail.com>
2024-11-20 09:47:29 +0100

    improved seek list (added color indicator, simplified time display, put time details in tooltip)

--- src/UI/SeekList.gd
+++ src/UI/SeekList.gd
@@ -4,6 +4,11 @@ class_name SeekList
 var seeks: Dictionary = {}
 var count: int = 0
 
+@export var iconBlack: Texture2D
+@export var iconWhite: Texture2D
+@export var iconBoth: Texture2D
+
+
 func _ready():
 	PlayTakI.ratingUpdate.connect(updateRatings)
 	PlayTakI.addSeek.connect(add)
@@ -16,15 +21,26 @@ func add(seek: SeekData, id: int):
 	tabButton.text = " Join Game (%d) " % count
 	var b = Button.new()
 	add_child(b)
-	b.text = " %-14s (%04d) %dx%d +%3.1f    %2d:00+:%02ds" % [seek.playerName, PlayTakI.ratingList.get(seek.playerName, 1000), seek.size, seek.size, seek.komi/2.0, seek.time/60, seek.increment]
+	#b.text = " %-14s (%04d) %dx%d +%3.1f    %2d:00+:%02ds" % [seek.playerName, PlayTakI.ratingList.get(seek.playerName, 1000), seek.size, seek.size, seek.komi/2.0, seek.time/60, seek.increment]
+	var time45 = seek.time + 45 * seek.increment
+	var type = "blitz" if time45 < 600 else "rapid" if time45 < 1200 else "classic"
+	b.text = "%s (%4d) %ds +%3.1f %s" % [seek.playerName, PlayTakI.ratingList.get(seek.playerName, 1000), seek.size, seek.komi/2.0, type]
+	b.tooltip_text = "%2d:00 +:%02d" % [seek.time / 60, seek.increment]
 	if seek.triggerTime != 0:
-		b.text += " +%2d@%d" % [seek.triggerTime/60, seek.triggerMove]
+		b.tooltip_text += " +%2d@%d" % [seek.triggerTime/60, seek.triggerMove]
+	
+	b.icon = [iconBoth, iconWhite, iconBlack][seek.color]
+	b.expand_icon = true
 	
 	if OS.has_feature("mobile"):
-		b.text = b.text.insert(24, "\n")
-		
+		b.autowrap_mode = TextServer.AUTOWRAP_WORD
+	else:
+		var v = get_theme_default_font().get_string_size(b.text, HORIZONTAL_ALIGNMENT_LEFT, -1, get_theme_default_font_size())
+		b.custom_minimum_size.x = v.x + v.y + 20 #ensure the icon is visible
+	
 	if seek.rated != SeekData.RATED:
-		b.text += " Unrated" if seek.rated == SeekData.UNRATED else " Tournament"
+		b.text += " Unrated" if seek.rated == SeekData.UNRATED else " Tournament" #TODO?
+	
 	seeks[id] = b
 	b.alignment = HORIZONTAL_ALIGNMENT_LEFT
 	b.pressed.connect(func(): accept(id))
@@ -52,6 +68,5 @@ func accept(id: int):
 
 func updateRatings():
 	for i in seeks:
-		var user = seeks[i].text.split(" ")[1]
-		seeks[i].text = Globals.ratingRegex.sub(seeks[i].text, "(%04d)" % PlayTakI.ratingList.get(user, 1000))
-		
+		var s = seeks[i].text.split(" ", false, 2)
+		seeks[i].text = s[0] + " (%4d) " % PlayTakI.ratingList.get(s[0], 1000) + s[2]
