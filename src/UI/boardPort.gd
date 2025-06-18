d2556b94c69d396cc02434537ec57b1dc0e1dec4
The1Rogue <fabiantschut@gmail.com>
2025-06-18 16:05:56 +0200

    fixed end game screen showing up over incorrect menus

--- src/UI/boardPort.gd
+++ src/UI/boardPort.gd
@@ -6,6 +6,13 @@ class_name BoardContainer
 
 @onready var UI = $GameUI
 @onready var Board = $Board
+@onready var EndGame = $End
+
+@onready var EndText: RichTextLabel = $End/VBoxContainer/Label
+@onready var EndLink: Button = $End/VBoxContainer/NinjaLink
+@onready var EndRematch: Button = $End/VBoxContainer/Rematch
+@onready var EndID: Button = $End/VBoxContainer/GameID
+
 
 @onready var boardHolder = $Board/SubViewport
 var board: BoardLogic
@@ -22,6 +29,13 @@ func set2D(v: bool, save: SettingData):
 
 func _ready() -> void:
 	GameLogic.setup.connect(setup)
+	
+	EndID.pressed.connect(func(): DisplayServer.clipboard_set(GameLogic.gameData.id))
+	EndLink.pressed.connect(func(): OS.shell_open("https://ptn.ninja/%s" % GameLogic.getPTN().replace("\n", " ")))
+	EndGame.gui_input.connect(func(e): if e is InputEventMouseButton: EndGame.hide())
+	EndRematch.pressed.connect(sendRematch)
+
+	GameLogic.end.connect(endGame)
 
 
 func setup(gameData: GameData, startState: GameState):
@@ -29,9 +43,11 @@ func setup(gameData: GameData, startState: GameState):
 
 
 func _draw():
+	EndGame.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
+	Board.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
+	
 	if Globals.isMobile():
 		UI.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
-		Board.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
 		
 		if $GameUI/MobilePTN.position.y == 0: #UI doesnt have a proper size yet, redraw when we know the size of it
 			queue_redraw.call_deferred()
@@ -44,7 +60,33 @@ func _draw():
 		
 	else:
 		UI.set_anchors_and_offsets_preset(PRESET_RIGHT_WIDE)
-		Board.set_anchors_and_offsets_preset(PRESET_FULL_RECT)
 		Board.set_anchor_and_offset(SIDE_RIGHT, 1, -UI.size.x)
 		$GameUI/MobilePTN.hide()
 		$GameUI/DesktopPTN.show()
+
+
+func endGame(type: int):
+	EndText.clear()
+	EndText.append_text(" Game over %s \n" % GameState.resultStrings[type])
+	
+	if type == GameState.DRAW:
+		EndText.append_text(" Game was drawn ")
+	elif type == GameState.ONGOING:
+		EndText.append_text(" Game is ongoing? (please report a bug) ")
+	else:
+		var p = GameLogic.gameData.playerWhiteName if type%2==0 else GameLogic.gameData.playerBlackName
+		EndText.push_color(Chat.color(p))
+		EndText.append_text(Chat.escape_bbcode(p))
+		EndText.pop()
+		EndText.append_text(" wins " + ["with a road ", "with flats ", "by default "][type/2])
+	
+	EndRematch.disabled = false
+	EndRematch.visible = (GameLogic.gameData.playerBlack == GameData.PLAYTAK and GameLogic.gameData.playerWhite == GameData.LOCAL) or (GameLogic.gameData.playerBlack == GameData.LOCAL and GameLogic.gameData.playerWhite == GameData.PLAYTAK)
+	EndGame.show()
+	Notif.ping(Notif.end)
+
+
+func sendRematch(): #TODO: local rematches?
+	EndRematch.disabled = true
+	Notif.ping()
+	PlayTakI.sendRematch()
